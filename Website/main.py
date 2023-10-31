a58ca450aeb899f9918b8b3b07e3f8f9cc61d046
Mr4500 <116310522+Mr4500@users.noreply.github.com>
2023-10-31 15:44:25 +0000

    Add files via upload
    Trick Or Treat Map additional work coming

--- Website/main.py
+++ Website/main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/TrickOrTreat')
+def TrickOrTreat():
+    return render_template('map.html')
+
 
 #host and port can be changed if needed to
 app.run(host='0.0.0.0', port=3025)
